5a014bdd18008aaab54a28615c7f49560b389d43
longlom <long.lom@yandex.ru>
2024-05-16 13:33:54 +0300

    FC: init

---
+++ converter_front/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col p-15">
+      <h1 className="mt-6 text-5xl m-auto">File converter</h1>
+
+      <div className="flex justify-between items-center flex-1">
+        <div className="outline-1 outline w-32 h-32 flex-1">ads</div>
+        <div className="outline-1 outline w-32 h-32 flex-1">ads</div>
+        <div className="outline-1 outline w-32 h-32 flex-1">ads</div>
+      </div>
+    </main>
+  );
+}
